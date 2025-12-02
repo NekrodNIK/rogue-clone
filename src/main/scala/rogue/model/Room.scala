6f2bd0256a3a6ec2f94690bb6692b2f314f22e54
whiteevilbro <maks-yakovlev21@yandex.ru>
2025-12-02 12:45:40 +0700

    rectangle boundaries fix

--- src/main/scala/rogue/model/Room.scala
+++ src/main/scala/rogue/model/Room.scala
@@ -1,22 +1,22 @@
 package rogue.model
 
 case class Rectangle(topLeft: Point, bottomRight: Point) {
-  def topRightCorner: Point = Point(bottomRight.x + 1, topLeft.y + 1)
-  def topLeftCorner: Point = Point(topLeft.x + 1, topLeft.y + 1)
+  def topRightCorner: Point = Point(bottomRight.x + 1, topLeft.y - 1)
+  def topLeftCorner: Point = Point(topLeft.x - 1, topLeft.y - 1)
   def bottomRightCorner: Point = Point(bottomRight.x + 1, bottomRight.y + 1)
-  def bottomLeftCorner: Point = Point(topLeft.x + 1, bottomRight.y + 1)
+  def bottomLeftCorner: Point = Point(topLeft.x - 1, bottomRight.y + 1)
   
   def topEdge: Iterator[Point] =
-    (topLeft.x + 1 until bottomRight.x).iterator.map(Point(_, topLeft.y - 1))
+    (topLeft.x to bottomRight.x).iterator.map(Point(_, topLeft.y - 1))
 
   def bottomEdge: Iterator[Point] =
-    (topLeft.x + 1 until bottomRight.x).iterator.map(Point(_, bottomRight.y + 1))
+    (topLeft.x to bottomRight.x).iterator.map(Point(_, bottomRight.y + 1))
 
   def leftEdge: Iterator[Point] =
-    (topLeft.y + 1 until bottomRight.y).iterator.map(Point(topLeft.x - 1, _))
+    (topLeft.y to bottomRight.y).iterator.map(Point(topLeft.x - 1, _))
 
   def rightEdge: Iterator[Point] =
-    (topLeft.y + 1 until bottomRight.y).iterator.map(Point(bottomRight.x + 1, _))
+    (topLeft.y to bottomRight.y).iterator.map(Point(bottomRight.x + 1, _))
 
   def innerPoints: Iterator[Point] =
     for
