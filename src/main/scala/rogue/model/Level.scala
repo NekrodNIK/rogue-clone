5c102f277bf65324cbb4e79e375c023a73b28de3
whiteevilbro <maks-yakovlev21@yandex.ru>
2025-12-02 13:43:56 +0700

    added coridor

--- src/main/scala/rogue/model/Level.scala
+++ src/main/scala/rogue/model/Level.scala
@@ -1,11 +1,15 @@
 package rogue.model
 
+import scala.collection.immutable.ListSet
 import scala.collection.mutable
 import scala.util.Random
+
+case class Corridor(points: ListSet[Point])
 
 case class Level(width: Int, height: Int) {
   private val maxrooms: Int = 2
   val rooms: mutable.ArraySeq[Room] = mutable.ArraySeq.fill(maxrooms)(null)
+  val corridors: mutable.ArraySeq[Corridor] = mutable.ArraySeq.fill(maxrooms * (maxrooms - 1) / 2)(null)
   regenerate()
 
   def regenerate(): Unit = {
@@ -19,7 +23,9 @@ case class Level(width: Int, height: Int) {
 //      rooms(i) = Room(Rectangle(topleft, topleft + size))
 //    }
     rooms(0) = Room(Rectangle(Point(11, 11), Point(21, 21)), Iterable(Point(16, 22)))
-    rooms(1) = Room(Rectangle(Point(5, 24), Point(25, 30)), Iterable(Point(16, 23)))
+    rooms(1) = Room(Rectangle(Point(5, 28), Point(25, 34)), Iterable(Point(16, 27)))
+    
+    corridors(0) = Corridor(ListSet.from(for i <- 23 to 26 yield Point(16, i)))
   }
 
   def contains(point: Point): Boolean = rooms.exists(_.contains(point))
