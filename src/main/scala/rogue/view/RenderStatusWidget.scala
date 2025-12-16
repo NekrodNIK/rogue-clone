f9c8932ac23df7f902e2f3b997d23e0e421bb97c
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-12-16 13:23:11 +0700

    fix

--- src/main/scala/rogue/view/RenderStatusWidget.scala
+++ src/main/scala/rogue/view/RenderStatusWidget.scala
@@ -6,7 +6,7 @@ import rogue.terminal
 
 class RenderStatusWidget(player: model.Player) extends RenderNode {
   override def children =
-    val str = s"GOLD: ${player.gold.toString}, HP: ${player.hp}".zipWithIndex
+    val str = s"GOLD: ${player.gold}, HP: ${player.hp}".zipWithIndex
     str.map((d, i) => RenderPoint(model.Point(i, 0), Symbol.Any(d)))
 
   override def render =
